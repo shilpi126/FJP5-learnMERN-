40e303c73de34e6fc3f4556b3daa4f9e8b447e69
shilpi <shilpi8624@gmail.com>
2022-11-01 15:43:40 +0530

    :goal_net: hoisting

---
+++ Javascript/hoisting.js
@@ -0,0 +1,23 @@
+console.log("line 1" + a);
+greet ();
+console.log("line 3" + hello);
+//hello();
+var a = 10;
+
+// function
+function greet() {
+    console.log("hello pepcoders");
+  }
+
+  //function expression 
+var hello = function () {
+    console.log("you guys are rock");
+}
+
+hello();
+
+// JavaScript Hoisting refers to the process whereby the interpreter appears to move the declaration of functions, variables to the top of their scope, prior to execution of the code.
+
+
+// my definiton
+//we can access the variables and function even before we have declared it; //  hoisting 
