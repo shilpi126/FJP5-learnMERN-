6ac21dcfd82937334032bd03cb69587cfd515abc
shilpi <shilpi8624@gmail.com>
2022-11-03 13:37:20 +0530

    :fire: axios, lifecycle method

--- React/movies/src/Components/List.js
+++ React/movies/src/Components/List.js
@@ -1,47 +1,113 @@
 import React, { Component } from 'react'
-import { movies } from './getMovies'
+//import { movies } from './getMovies'
+import axios from "axios"
+import API_KEY from '../secrets';
 export default class List extends Component {
+  constructor () {
+    super ();
+    //console.log("constructor is called");
+    this.state = {
+      hover: "",
+      parr: [1],
+      currPage: 5,
+      movies:[],
+    };
+  }
+
+  handleEnter = (id) => {
+    this.setState({
+      hover: id,
+    });
+  };
+
+  handleLeave = () => {
+    this.setState({
+      hover: "",
+    });
+  };
+
+   async componentDidMount() {
+
+    //console.log("componentDidMount is called");
+    //console.log(API_KEY);
+    let ans = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currPage}`
+      );
+    //console.log(ans.data);
+    this.setState({
+      movies:[...ans.data.results]//[{},{},{}]
+    });
+    }
+
   render() {
- let movie = movies.results;
+    //console.log("render is called");
+    //let movie = movies.results;
     return (
        <>
-      {movie.length == 0 ? 
-      (
+
+      {this.state.movies.length == 0 ? (
         <div className="spinner-grow text-success" role="status">
             <span className="visually-hidden">Loading...</span>
           </div>
       ):(
+
         <div>
         <h3 className='text-center'>
           <strong>Treanding</strong>
         </h3>
 
-        <div className='movie-list'>
-          {movie.map((movieObj) => (
-             <div className="card movie-card">
+
+        <div className='movies-list'>
+         {this.state.movies.map((movieObj) => (
+             <div 
+             className="card movie-card"
+             onMouseEnter={() => this.handleEnter(movieObj.id)}
+             onMouseLeave = {this.handleLeave}
+             >
              <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} className="card-img-top banner-img" alt="..." style={{height:"40vh", width:"20vw" }}/>
              {/* <div className="card-body"> */}
                <h5 className="card-title movie-title">{movieObj.original_title}</h5>
                {/* <p className="card-text movie-text">{movieObj.overview}</p> */}
                 <div className="button-wrapper">
-                <a href="#" className="btn btn-primary movie-button">Go somewhere</a> 
+                  { this.state.hover == movieObj.id &&  
+                <a href="#" className = "btn btn-primary movie-button"
+                  
+                >Add To Favorite</a> 
+              }
               </div>
              </div>
-          //  </div>
+          
           ))}
         </div>
-            <nav aria-label="Page navigation example">
-              <ul class="pagination">
-                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item"><a class="page-link" href="#">Next</a></li>
-              </ul>
-            </nav>
-        </div>
-      )}
+         <div className='pagination'>  
+          <nav aria-label="Page navigation example">
+            <ul className="pagination">
+                <li className="page-item">
+                  <a className="page-link" href="#">Previous</a>
+                </li>
+                
+                
+              
+                {this.state.parr.map((pageNum) => (//jsx + html use krna hai isliye ye bracket () use karenge.
+                  
+                <li className="page-item">
+                  <a className="page-link" href="#" >
+                    {pageNum}
+                  </a>
+                </li>
+                ))
 
+               }
+
+              <li className="page-item">
+                  <a className="page-link" href="#">Next</a>
+              </li>
+             </ul>
+           </nav>
+          </div>
+        </div> 
+             
+      )}
     </>
 
     )
