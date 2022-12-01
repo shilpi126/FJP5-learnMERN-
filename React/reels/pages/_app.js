e5d8f729efc22941f8bc6005d147d7337d2ecdb5
shilpi <shilpi8624@gmail.com>
2022-12-01 17:17:50 +0530

    :lipstick: navbar and upload UI

--- React/reels/pages/_app.js
+++ React/reels/pages/_app.js
@@ -2,9 +2,10 @@ import '../styles/globals.css'
 import './signup/signup.css'
 import './login/login.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
+import '../components/Feed.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+  
 }
 
 export default MyApp
